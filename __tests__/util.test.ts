5e616b19b0e358e3953251e822fe61288458eaa4
John <bxb100@outlook.com>
2022-01-22 01:20:21 +0800

    chore: change image name

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,15 +1,13 @@
 import {filePaths, unmatchedPatterns} from '../src/util'
 import * as assert from 'assert'
-import * as glob from '@actions/glob'
 import * as path from 'path'
-
 
 describe('util', () => {
     describe('paths', () => {
         it('expect github glob function', async () => {
             assert.deepStrictEqual(
                 await filePaths(['test/data/**/*', 'test/data/does/not/exist/*']),
-                [path.resolve(__dirname, '../test/data/foo/童贞法师.jpg')]
+                [path.resolve(__dirname, '../test/data/foo/test-imge.jpg')]
             )
         })
     })
